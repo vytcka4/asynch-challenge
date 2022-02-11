30418da067beffcf42f391003eac509474213948
vytcka4 <vytautas.freelance@gmail.com>
2022-02-11 05:27:58 +0200

    commit

--- script.js
+++ script.js
@@ -1,45 +1,117 @@
-const btn = document.querySelector(".btn-country");
-const countriesContainer = document.querySelector(".countries");
+const images = document.querySelector(".images");
 
-const renderCountry = function (data, className = "") {
-  const html = `
-  <article class="country ${className}">
-    <img class="country__img" src="${data.flag}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-    </div>
-  </article>
-  `;
-  countriesContainer.insertAdjacentHTML("beforeend", html);
-  countriesContainer.style.opacity = 1;
-};
-//
+// const btn = document.querySelector(".btn-country");
+// const countriesContainer = document.querySelector(".countries");
 
-const whereIAm = function (lat, lng) {
-  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-    .then((resp) => {
-      if (!resp.ok) {
-        throw new Error("somenthing went wrong");
-      }
-      return resp.json();
-    })
-    .then((data) => {
-      console.log(data);
-      console.log(`you are in ${data.city}, ${data.country}`);
-      return data;
-    })
-    .then((data) => {
-      renderCountry([data.latt, data.longt]);
-    })
-    .catch((err) => console.error(err));
-};
+// const renderCountry = function (data, className = "") {
+//   const html = `
+//   <article class="country ${className}">
+//     <img class="country__img" src="${data.flag}" />
+//     <div class="country__data">
+//       <h3 class="country__name">${data.name}</h3>
+//       <h4 class="country__region">${data.region}</h4>
+//       <p class="country__row"><span>👫</span>${(
+//         +data.population / 1000000
+//       ).toFixed(1)} people</p>
+//       <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//       <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//     </div>
+//   </article>
+//   `;
+//   countriesContainer.insertAdjacentHTML("beforeend", html);
+//   countriesContainer.style.opacity = 1;
+// };
+// //
 
-whereIAm(52.508, 13.381);
+// const whereIAm = function (lat, lng) {
+//   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//     .then((resp) => {
+//       if (!resp.ok) {
+//         throw new Error("somenthing went wrong");
+//       }
+//       return resp.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       console.log(`you are in ${data.city}, ${data.country}`);
+//       return data;
+//     })
+//     .then((data) => {
+//       renderCountry([data.latt, data.longt]);
+//     })
+//     .catch((err) => console.error(err));
+// };
+
+// whereIAm(52.508, 13.381);
 
 // latt longt
+
+// For this challenge you will actually have to watch the video! Then, build the image
+// loading functionality that I just showed you on the screen.
+// Your tasks:
+// Tasks are not super-descriptive this time, so that you can figure out some stuff by
+// yourself. Pretend you're working on your own �
+// PART 1
+// 1. Create a function 'createImage' which receives 'imgPath' as an input.
+// This function returns a promise which creates a new image (use
+// document.createElement('img')) and sets the .src attribute to the
+// provided image path
+// 2. When the image is done loading, append it to the DOM element with the
+// 'images' class, and resolve the promise. The fulfilled value should be the
+// image element itself. In case there is an error loading the image (listen for
+// the'error' event), reject the promise
+// 3. If this part is too tricky for you, just watch the first part of the solution
+// PART 2
+// 4. Consume the promise using .then and also add an error handler
+// 5. After the image has loaded, pause execution for 2 seconds using the 'wait'
+// function we created earlier
+// 6. After the 2 seconds have passed, hide the current image (set display CSS
+// property to 'none'), and load a second image (Hint: Use the image element
+// returned by the 'createImage' promise to hide the current image. You will
+// need a global variable for that �)
+// 7. After the second image has loaded, pause execution for 2 seconds again
+// 8. After the 2 seconds have passed, hide the current image
+
+console.log(images);
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = imgPath;
+    img.addEventListener("load", function () {
+      console.log(img);
+      images.append(img);
+      resolve(img);
+    });
+    img.addEventListener("error", function () {
+      reject(new Error("image not found"));
+    });
+  });
+};
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+let currentImg;
+
+createImage("img/img-1.jpg")
+  .then((img) => {
+    currentImg = img;
+    console.log(img);
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = "none";
+    return createImage("img/img-2.jpg").then((img) => {
+      currentImg = img;
+      return wait(2);
+    });
+  })
+  .then(() => {
+    currentImg.style.display = "none";
+    return createImage("img/img-3.jpg").then((img) => {});
+  })
+  .catch((err) => console.log(err));
+//
